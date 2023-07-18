8a761e3f479f434eb1fd84f346ff6503ee9af7b5
Shubham <shubhamkashyap0608@gmail.com>
2023-07-18 15:12:51 +0530

    Configured speech to text

---
+++ speechToText.py
@@ -0,0 +1,29 @@
+import speech_recognition as sr
+
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="Path-to-credentials file"
+#GOOGLE_APPLICATION_CREDENTIALS= "Users/Shubham/Downloads/bubbly-monument-393208-80736f7e062b.json"
+
+# Create a recognizer
+recognizer = sr.Recognizer()
+
+# Initialize a microphone instance
+mic = sr.Microphone()
+
+# Start listening to the microphone
+with mic as source:
+    recognizer.adjust_for_ambient_noise(source)
+    print("Say something!")
+    audio = recognizer.listen(source)
+
+# Recognize speech
+try:
+    # Convert the audio to text
+    text = recognizer.recognize_google_cloud(audio, language="en-US")
+    print(f"You said: {text}")
+except sr.UnknownValueError:
+    print("I didn't understand what you said.")
+except sr.RequestError as e:
+    print(f"Error: {e}")
+
+inpLanTxt = text
